4f7261d6c7bd4cbaa8e54cc15435431a8b6445ec
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2023-01-03 11:13:45 +0000

    commit

--- 04-Three.js/lesson02/materials.js
+++ 04-Three.js/lesson02/materials.js
@@ -1,13 +1,5 @@
 import * as THREE from "https://threejs.org/build/three.module.js";
 
-const redPlastic = new THREE.MeshPhongMaterial();
-redPlastic.color = new THREE.Color(0.5, 0.0, 0.0);
-redPlastic.specular= new THREE.Color(0.7, 0.6, 0.6);
-redPlastic.shininess = 0.25 * 256;
-redPlastic.ambient = new THREE.Color(0.0, 0.0, 0.0);
-export {redPlastic};
-
-
 const blueBasicMaterial = new THREE.MeshPhongMaterial({
     color: 0x0000FF,
 });
@@ -30,3 +22,41 @@ const redTransparent = new THREE.MeshPhongMaterial({
 	transparent: true,
 });
 export {redTransparent};
+
+const emerald = new THREE.MeshPhongMaterial({ shading: THREE.SmoothShading });
+emerald.color = new THREE.Color(0.07568, 0.61424, 0.07568);
+emerald.specular= new THREE.Color(0.633, 0.7278, 0.633);
+emerald.shininess = 0.6 * 256;
+emerald.ambient = new THREE.Color(0.0215, 0.1745, 0.0215);
+export {emerald};
+
+const redPlastic = new THREE.MeshPhongMaterial();
+redPlastic.color = new THREE.Color(0.5, 0.0, 0.0);
+redPlastic.specular= new THREE.Color(0.7, 0.6, 0.6);
+redPlastic.shininess = 0.25 * 256;
+redPlastic.ambient = new THREE.Color(0.0, 0.0, 0.0);
+export {redPlastic};
+
+const whiteRubber = new THREE.MeshPhongMaterial({ shading: THREE.SmoothShading });
+whiteRubber.color = new THREE.Color(0.5, 0.5, 0.5);
+whiteRubber.specular= new THREE.Color(0.7, 0.7, 0.7);
+whiteRubber.shininess = 0.0781 * 256;
+whiteRubber.ambient = new THREE.Color(0.05, 0.05, 0.05);
+export {whiteRubber};
+
+const glassMaterial = new THREE.MeshPhongMaterial( { 
+    color: 0x222222, 
+    specular: 0xFFFFFF,
+    shininess: 100, 
+    opacity: 0.3, 
+    transparent: true 
+} );
+export {glassMaterial};
+
+const blueTransparent = new THREE.MeshPhongMaterial({ 
+    color: 0x0000FF,
+    shininess: 100,
+    opacity: 0.3,
+    transparent: true,
+});
+export {blueTransparent};
